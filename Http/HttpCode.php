9c7d8592f39bbc1e609d00ca7af35d41b7ded725
Dmitriy Shuba <sda@sda.in.ua>
2016-08-15 15:41:20 +0300

    Remove codes.

--- Http/HttpCode.php
+++ Http/HttpCode.php
@@ -19,14 +19,11 @@ abstract class HttpCode
     const POST_SUCCESS_CODE            = 201;
     const POST_SUCCESS_MESSAGE         = 'Created';
 
-    const POST_CREATED_CODE            = 201;
-    const POST_CREATED_MESSAGE         = 'Created';
+    const POST_FAIL_CODE               = 400;
+    const POST_FAIL_MESSAGE            = 'Bad Request';
 
     const PUT_SUCCESS_CODE             = 202;
     const PUT_SUCCESS_MESSAGE          = 'Updated';
-
-    const POST_FAIL_CODE               = 400;
-    const POST_FAIL_MESSAGE            = 'Bad Request';
 
     const PUT_FAIL_CODE                = 400;
     const PUT_FAIL_MESSAGE             = 'Bad Request';
